86595265997877991cee69aa0cda7b0b7dd6fef4
Javed Wright <jdwright870@gmail.com>
2017-10-11 16:17:10 -0500

    Exercise 2

--- maze.js
+++ maze.js
@@ -8,4 +8,26 @@ function main(){
 		this.className += " youlose";
 	});
 	//==================================================================
+	//Exercise 2
+	let boundaries = document.querySelectorAll(".boundary");
+	let e_flag = -1;
+
+	for(var i = 0; i < boundaries.length; i++){
+		if(boundaries[i].className.indexOf("example") != -1){
+			boundaries[i].addEventListener("mouseover", function(){
+				this.className = "boundary example youlose"
+			})
+			e_flag = i;
+		}else{
+			boundaries[i].addEventListener("mouseenter", function(){
+				for(var i = 0; i < boundaries.length; i++){
+					if( i != e_flag){
+						boundaries[i].className = "boundary youlose";
+					}
+				}
+			});
+		}
+	}
+	//=================================================================
+
 }

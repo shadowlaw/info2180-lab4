32b974b02c1089c0822efd41baf9aeec19dfae78
Javed Wright <jdwright870@gmail.com>
2017-10-11 16:26:54 -0500

    Updated Excercise 1 and 2.

--- maze.js
+++ maze.js
@@ -5,7 +5,7 @@ function main(){
 	//Exercise 1
 	let wall = document.getElementById("boundary1");
 	wall.addEventListener("mouseover", function(){
-		this.className += " youlose";
+		this.className = "boundary youlose";
 	});
 	//==================================================================
 	//Exercise 2
@@ -14,12 +14,9 @@ function main(){
 
 	for(var i = 0; i < boundaries.length; i++){
 		if(boundaries[i].className.indexOf("example") != -1){
-			boundaries[i].addEventListener("mouseover", function(){
-				this.className = "boundary example youlose"
-			})
 			e_flag = i;
 		}else{
-			boundaries[i].addEventListener("mouseenter", function(){
+			boundaries[i].addEventListener("mouseover", function(){
 				for(var i = 0; i < boundaries.length; i++){
 					if( i != e_flag){
 						boundaries[i].className = "boundary youlose";
